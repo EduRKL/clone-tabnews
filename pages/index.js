2f8e69da6d9f270bbffe3efdf2749d9a2129f210
Eduardo Lima <106208300+EduRKL@users.noreply.github.com>
2023-07-18 01:48:58 +0000

    adiciona scripts `lint:check` e `lint:fix` e o `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Bru, minha princesinha, se você me ama... dá uma risadinha</h1>
+function Home() {
+  return <h1>Bru, minha princesinha, se você me ama... dá uma risadinha</h1>;
+}
+
+function teste() {
+  console.log("identação errada");
 }
 
 export default Home;
