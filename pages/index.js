43b24168d459a2937ce9c9e49c1b31f57af4f25d
Eduardo Lima <106208300+EduRKL@users.noreply.github.com>
2023-07-18 01:50:54 +0000

    limpeza do `index.js`

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,4 @@ function Home() {
   return <h1>Bru, minha princesinha, se você me ama... dá uma risadinha</h1>;
 }
 
-function teste() {
-  console.log("identação errada");
-}
-
 export default Home;
