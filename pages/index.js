94ed40752ed8423b5a64856e13a016aeeaaaee63
Eduardo Lima <106208300+EduRKL@users.noreply.github.com>
2025-07-03 20:25:57 +0000

    Mensagem alterada para teste

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-  return <h1>Bru, minha princesinha, se você me ama... dá uma risadinha</h1>;
+  return (
+    <h1>Bru, minha princesinha, se você me ama... dá uma risadinha TESTE</h1>
+  );
 }
 
 export default Home;
